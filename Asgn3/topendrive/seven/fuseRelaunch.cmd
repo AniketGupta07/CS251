b50fa4a82e3f02251ff899de8a9fd76939b30ff2
Ani <aniketg417@gmail.com>
2019-01-26 19:05:22 +0530

    .

--- Asgn3/topendrive/seven/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "/users/btech/aniketg/topendrive/seven/seven_top_isim_beh.exe" -prj "/users/btech/aniketg/topendrive/seven/seven_top_beh.prj" "work.seven_top" "work.glbl" 
