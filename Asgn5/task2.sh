8add56e8836140eb3f617ecd97bd04734683fb94
Ani <aniketg417@gmail.com>
2019-02-12 18:58:17 +0530

    gprof task

---
+++ Asgn5/task2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+gcc -o sum -Wall -pg sum.c 
+./sum
+gprof -bp sum gmon.out | sed '1,5d' | sed 's\  *\ \g' | awk '{print $3F,$NF}' >  flat
+rm -rf final
+declare -A arr
+
+while read c1 c2 leftovers;do
+	arr[$c2]=$c1;
+done < flat   
+
+gprof -bP sum gmon.out | sed '1,7d' > callgraph
+let count=0;
+while [[ true ]]; do
+	cat callgraph | sed '/---/q' > temp;
+	NUMOFLINES=$(wc -l < "temp");
+	if cmp -s "temp" "callgraph" ; then
+   		break;
+   	fi
+	 sed -i "1,${NUMOFLINES}d" callgraph;
+	 let count++;
+	sed -ni '/\['"${count}"'\]/,$p' temp;
+	 sed -i '$ d' temp;
+	 awk '{print $(NF-1)}' temp > func;
+	 main=$(head -n 1 func)
+	 sed -i '1d' func;
+	 while [[ true ]]; do
+		
+	    if [ ! -s "func" ] ; then
+	    	break;
+	    fi
+	    
+	    sub=$(head -n 1 func);
+	     sed -i '1d' func;
+	     echo $main,${arr[$main]},$sub,${arr[$sub]} >> final ;
+	 done
+done
+rm -rf callgraph flat temp
