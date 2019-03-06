79f4ea329066d888478b5eab8938ed483736753f
nmnjindal <naman.excellent@gmail.com>
2019-03-06 22:52:18 +0530

    Assignment 7 updated

--- Asgn7/assgn7.hs
+++ Asgn7/assgn7.hs
@@ -8,27 +8,12 @@ fib_temp a b c n = if n == 0
                         fib_temp (b+a) a (c+1) n
 
 fib n = fib_temp 1 0 1 n
+
 fib_inf = [fib i | i<-[1..]]
 
 
-is_prime x n  = if x <= 1 
-                then 0 
-                else if x == n 
-                        then 1 
-                else if (mod x n) == 0 
-                        then 0 
-                else is_prime x (n+1)
+modu n = [x | x<-[2..n-1], n`mod`x ==0]
+isprime n  |length(modu n) == 0 =1 |otherwise =0
+inf = [ x | x<-[2..], isprime(x)==1]
 
-prime_tmp x c n = if n <= 0 
-                then -1 
-                else if (((is_prime x 2) == 1 ) && c == n)
-                        then x 
-                else if (is_prime x 2 == 1 ) 
-                        then prime_tmp (x+1) (c+1) n 
-                else prime_tmp (x+1) c n
-
-prime n = prime_tmp 1 1 n
-
-prime_inf = [prime i | i<-[1..]]
-
-
+prime n = inf!!(n - 1 )
